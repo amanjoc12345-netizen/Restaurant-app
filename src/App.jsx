17e65b8d54c3af368286b1994ab90fdc13b6f34f
Aman <amanjoc12345@gmail.com>
2026-06-09 21:12:26 +0530

    Added restaurant summary section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Header from "./components/Header";
+import MealsSummary from "./components/MealsSummary";
 
 function App() {
   return (
     <>
     <Header />
+    <MealsSummary />
     </>
-  )
+  );
 }
 export default App;
---
+++ src/components/MealsSummary.jsx
@@ -0,0 +1,18 @@
+import "./MealsSummary.css";
+function MealsSummary(){
+    return (
+        <section className = "summary">
+        <h2>Delicious Food, Delivered to you</h2>
+
+        <p>   
+                 Choose your favourite meal from our broad selection of availabel meals and enjoy a delicious lunch or dinner at home.
+        </p>
+
+        <p>
+            All our meals are cooked with high-quality ingredients, just-in-time and of course by experienced chefs!
+        </p>
+
+        </section>
+    );
+}
+export default MealsSummary;
