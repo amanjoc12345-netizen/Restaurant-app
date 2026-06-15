5e8da84edb772243b96fc88e618ac4a201a63406
Aman <amanjoc12345@gmail.com>
2026-06-15 10:41:02 +0530

    Added hardcoded cart modal using React Portal

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Header from "./components/Header";
 import MealsSummary from "./components/MealsSummary";
 import AvailabelMeals from "./components/AvailabelMeals";
+import Cart from "./components/cart";
 
 function App() {
   return (
     <>
+    <Cart/>
     <Header />
     <MealsSummary />
     <AvailabelMeals />
---
+++ src/components/cart.jsx
@@ -0,0 +1,26 @@
+import { createPortal } from "react-dom";
+import "./cart.css";
+
+const Cart = () => {
+  return createPortal(
+    <div className="backdrop">
+      <div className="modal">
+        <p>Sushi</p>
+        <br></br>
+
+        <div className="total">
+          <h2>Total Amount</h2>
+          <h2>Rs. 500</h2>
+        </div>
+
+        <div className="actions">
+          <button className="button--alt">Close</button>
+          <button className="button--alt">Order</button>
+        </div>
+      </div>
+    </div>,
+    document.getElementById("overlays")
+  );
+};
+
+export default Cart;
